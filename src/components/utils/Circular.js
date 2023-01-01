f00805691c454ff9eb632f155bd50978c184ff1a
David S. Batista <dsbatista@gmail.com>
2023-01-01 07:03:07 +0000

    adding circular progress

---
+++ src/components/utils/Circular.js
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
+function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress />
+    </Box>
+  )
+}
+
+export default CircularIndeterminate
