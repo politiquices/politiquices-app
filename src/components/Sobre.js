859773de321b070fe2cfe25df1972ecb5aaf065f
David S. Batista <dsbatista@gmail.com>
2023-02-08 02:28:56 +0100

    adding FAQ questions

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -8,8 +8,13 @@ function Sobre() {
       <Typography sx={{ paddingTop: 10 }} align="center">
         Perguntas Frequentes
       </Typography>
-      <Typography align="center">Porque é que algumas personalidades não têm foto?</Typography>
+      <Typography align="center"> - Porque é que algumas personalidades não têm foto?</Typography>
       <Typography align="center">Algumas personalidades têm a informação pessoal errada ou incompleta</Typography>
+      <Typography align="center"> - Porque é que apenas há dados até 2021 ?</Typography>
+      <Typography align="center">
+        Os dados usados pelo politiquices.pt provêm do Arquivo.PT que apenas pública as páginas um ano depois de terme
+        sido arquivadas
+      </Typography>
 
       <Box align="center" sx={{ paddingTop: 10 }}>
         <Typography align="center">Apresentação Café com Arquivo.PT 2022</Typography>
