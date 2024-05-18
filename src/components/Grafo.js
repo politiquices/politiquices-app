349e01cc84bfb5091596f88d2ca44ce27975ccc7
David S. Batista <dsbatista@gmail.com>
2024-05-18 11:07:30 +0100

    wip: interaction with graph

--- src/components/Grafo.js
+++ src/components/Grafo.js
@@ -22,7 +22,9 @@ const maxYear = 2024
 
 function VisNetwork() {
   const container = useRef(null)
+
   // see: https://visjs.github.io/vis-network/docs/network/#options
+  
   const options = {
     physics:{
         enabled: true,
@@ -72,6 +74,7 @@ function VisNetwork() {
         sortMethod: 'hubsize'
       }
     },
+
   };
 
   const [loading, setLoading] = useState(false)
@@ -176,9 +179,121 @@ function VisNetwork() {
   useEffect(() => {
     console.log("calling useEffect 2")
   	// Use `network` here to configure events, etc.
-    const network = container.current && new Network(container.current, { nodes, edges }, options)
-  }, [container, nodes, edges])
-  
+
+    const network = container.current && new Network(container.current, { nodes, edges }, options);
+
+    /*
+    network.on('click', (params) => {
+        
+      params.event.preventDefault();
+      pointer = params['pointer']['DOM']
+      node = network.getNodeAt(pointer)
+      edge = network.getEdgeAt(pointer)
+
+      // clicked nothing
+      if (node == null && edge == null) {
+        $(".custom-menu").hide();
+      }
+
+      // clicked a Node
+      else if ((node != null && edge != null) || (node != null && edge == null)) {       
+        var node_id = node;
+        var node_name = network.body.nodes[node_id].options['label']
+        var hostname = window.location.origin
+        var url = `${hostname}/entity?q=${node_id}`
+        var entity_html = `<a href=${url} target="_blank">${node_name}</a>`
+        $(".custom-menu").find('#noticias').html(entity_html);
+        $(".custom-menu").find('#ent1').html('');
+        $(".custom-menu").find('#rel_type').html('');
+        $(".custom-menu").find('#ent2').html('');
+        $(".custom-menu").css({
+          top: params['event']['center']['y'] + "px",
+          left: params['event']['center']['x'] + "px"
+        });
+        $(".custom-menu").show()
+      }
+      
+      // clicked an Edge
+      else if (node == null && edge != null) {          
+        var edge_id = edge
+        var rel_text = network.body.edges[edge_id].options.title;
+        var rel_type = rel_type_map[rel_text]
+        var freq = network.body.edges[edge_id].options.value
+        var from = network.body.edges[edge_id]["fromId"]
+        var to = network.body.edges[edge_id]["toId"]
+        var from_name = network.body.nodes[from].options['label']
+        var to_name = network.body.nodes[to].options['label']
+        var hostname = window.location.origin
+        var year_from = $("#ano_from").text();
+        var year_to = $("#ano_to").text();
+        var url = `${hostname}/queries?&e1=${from}&e2=${to}&year_from=$
+        {year_from}&year_to=${year_to}&relationship=${rel_type}`
+        var noticias_html = `<a href=${url} target="_blank">${freq} notícias</a>`
+        var e1_url = `${hostname}/entity?q=${from}`
+        var e1_html = `<a href=${e1_url} target="_blank">${from_name}</a>`
+        var e2_url = `${hostname}/entity?q=${to}`
+        var e2_html = `<a href=${e2_url} target="_blank">${to_name}</a>`
+        rel_html = `<b>${rel_text}</b>`
+        $(".custom-menu").find('#noticias').html(noticias_html);
+        $(".custom-menu").find('#ent1').html(e1_html);
+        $(".custom-menu").find('#rel_type').html(rel_html);
+        $(".custom-menu").find('#ent2').html(e2_html);
+        $(".custom-menu").css({
+          top: params['event']['center']['y'] + "px",
+          left: params['event']['center']['x'] + "px"
+        });
+        $(".custom-menu").show()
+
+      }
+
+    });
+    */
+
+    /*
+    const showPopup = (nodeData, nodePosition) => {
+      const box = document.createElement('div');
+      box.style.position = 'absolute';
+      box.style.left = `${nodePosition.left + container.current.offsetLeft}px`;
+      box.style.top = `${nodePosition.top + container.current.offsetTop}px`;
+      box.style.padding = '10px';
+      box.style.backgroundColor = 'white';
+      box.style.border = '1px solid black';
+      box.style.zIndex = 1000;
+
+      const content = document.createElement('div');
+      content.innerText = `Node Data: ${JSON.stringify(nodeData, null, 2)}`;
+      
+      const link = document.createElement('a');
+      link.href = 'https://www.publico.pt';
+      link.target = '_blank';
+      link.innerText = 'Go to Publico.pt';
+      
+      box.appendChild(content);
+      box.appendChild(link);
+      
+      document.body.appendChild(box);
+
+      setTimeout(() => {
+        document.body.removeChild(box);
+      }, 5000);
+    };
+    */
+
+    network.on('click', (params) => {
+      if (params.nodes.length > 0) {
+        const nodeId = params.nodes[0];
+        // const nodePosition = network.getPositions([nodeId])[nodeId];
+        // console.log(nodePosition);
+        // window.open('https://www.publico.pt', '_blank');
+        const nodeData = nodes.get(nodeId);
+        console.log(nodeId);
+        console.log(nodeData);
+        // showPopup(nodeData, network.getBoundingBox(nodeId));
+      }
+    });
+
+  }, [container, nodes, edges]);
+
   return (
     <>
     <Box alignItems="center">
