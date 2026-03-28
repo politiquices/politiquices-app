c086ee873caae35e1e1a7ba3e0453c1bd694fb52
David S. Batista <dsbatista@gmail.com>
2026-03-28 09:34:14 +0100

    set cache control: no store

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
   optimizeDeps: {
     include: ['recharts'],
     esbuildOptions: {
